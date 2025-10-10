63a320504b6e59161aa2187b99104b56154cb407
ctw02371 <ctw02371@criticaltechworks.com>
2025-10-10 11:45:30 +0100

    Created transaction entity

---
+++ server/src/main/java/pt/fabioazevedo/transaction/TransactionEntity.java
@@ -0,0 +1,36 @@
+package pt.fabioazevedo.transaction;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "transactions", schema = "finapenses")
+@NoArgsConstructor
+@Getter
+@Setter
+public class TransactionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "amount", precision = 10, scale = 2, nullable = false)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "transaction_type", nullable = false)
+    private TransactionType transactionType;
+
+    @Column(name = "transaction_date", nullable = false)
+    private LocalDate date;
+}
---
+++ server/src/main/java/pt/fabioazevedo/transaction/TransactionRepository.java
@@ -0,0 +1,10 @@
+package pt.fabioazevedo.transaction;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.UUID;
+
+@ApplicationScoped
+public class TransactionRepository implements PanacheRepositoryBase<TransactionEntity, UUID> {
+}
